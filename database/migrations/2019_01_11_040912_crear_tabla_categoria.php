40b398282377a57b86b9563a46780fb74692b321
Henry Iván Peña Contreras <42283812+pecons@users.noreply.github.com>
2019-01-10 23:28:47 -0500

    Se creó migración para crear tabla categoria.

---
+++ database/migrations/2019_01_11_040912_crear_tabla_categoria.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaCategoria extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      //se crea la tabla categoria
+        Schema::create('categoria', function (Blueprint $table) {
+            $table->increments('pk_categoria');
+            $table->string('cat_nombre', 50);
+            $table->string('cat_descripcion', 140)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      //hace drop de la tabla Categoria
+        Schema::dropIfExists('categoria');
+    }
+}
