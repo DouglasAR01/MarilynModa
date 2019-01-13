2b00f1927d2fd7677e067e3d1c0397b09942d93d
Marianne Rojas <mariannesolangel6a@gmail.com>
2019-01-13 09:26:19 -0500

    Se creó la mig crear_tabla_prenda_pal_clave

---
+++ database/migrations/2019_01_13_141743_crear_tabla_prenda_pal_clave.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaPrendaPalClave extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('prenda_pal_clave', function (Blueprint $table) {
+          $table->unsignedInteger('ppc_fk_prenda');
+          $table->unsignedInteger('ppc_fk_palabra_clave');
+
+          $table->foreign('ppc_fk_prenda')
+                ->references('pk_prenda')->on('prenda');
+
+          $table->foreign('ppc_fk_palabra_clave')
+                ->references('pk_palabra_clave')->on('palabra_clave');
+
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prenda_pal_clave');
+    }
+}
