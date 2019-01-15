77e02ea5273bf8d709943bd80e7103d24d3f529f
Marianne Rojas <mariannesolangel6a@gmail.com>
2019-01-15 13:32:00 -0500

    Adaptación de la navbar de Laravel y  adición del footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Bootstrap CSS and other CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     @yield('stylesheets')
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <title>Marilyn Moda</title>
   </head>
 
-  <body>
+  <body id="bootstrap-overrides">
     <div class="container-fluid">
       <!--HEADER-->
       <div class="row header">
@@ -26,21 +35,61 @@
       </div>
 
       <!--NAVIGATION-->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Marylin Moda</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-li"><a href="/" class="nav_item">Inicio</a></li>
-            <li class="nav-li"><a href="catalogo" class="nav_item">Catálogo</a></li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-laravel">
+          <div class="container">
+              <a class="navbar-brand" href="{{ url('/') }}">
+                  {{-- {{ config('app.name', 'Laravel') }} --}}
+                  MarilynModa
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <!-- Left Side Of Navbar -->
+                  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav_item"><a href="/" class="nav_link">Inicio</a></li>
+                    <li class="nav_item"><a href="catalogo" class="nav_link">Catálogo</a></li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+
+                  <!-- Right Side Of Navbar -->
+                  <ul id="nav-right" class="navbar-nav mt-2 mt-lg-0">
+                      <!-- Authentication Links -->
+                      @guest
+                          <li class="nav_item">
+                              <a class="nav_link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                          </li>
+                          @if (Route::has('register'))
+                              <li class="nav_item">
+                                  <a class="nav_link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                              </li>
+                          @endif
+                      @else
+                          <li class="nav_item dropdown">
+                              <a id="navbarDropdown" class="nav_link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  {{ Auth::user()->name }} <span class="caret"></span>
+                              </a>
+
+                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <a class="dropdown-item" href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                  </form>
+                              </div>
+                          </li>
+                      @endguest
+                  </ul>
+              </div>
+          </div>
       </nav>
 
       <!--CONTENT-->
@@ -49,9 +98,45 @@
       </div> <!--END OF CONTENT-->
 
       <!--FOOTER-->
-      <footer class="text-center row">
-          <a href="#Top" class="fa top">TOP ↑ </a><a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a><a
-              href="#" class="fa fa-google"></a><a href="#" class="fa fa-youtube"></a>
+
+      <footer class="row text-center">
+        <div class="footer-grid">
+          <ul class="footer-grid__item f_row f_row_ul">
+            <li><a href="#" >Volver Arriba</a></li>
+            <li><a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a><a href="#" class="fa fa-google"></a><a href="#" class="fa fa-youtube"></a></li>
+          </ul>
+
+          <div class="footer-nav footer-grid__item f_middle">
+            <h6 class="footer-nav__title">Nosotros</h6>
+            <ul class="footer-nav__submenu">
+              <li><a class="footer-nav__link" href="#">Acerca de nosotros</a></li>
+              <li><a class="footer-nav__link" href="#">Trabaja con nosotros</a></li>
+              <li><a class="footer-nav__link" href="#">Contáctanos</a></li>
+            </ul>
+          </div>
+
+          <div class="footer-nav footer-grid__item f_middle">
+            <h6 class="footer-nav__title">Información</h6>
+            <ul class="footer-nav__submenu">
+              <li><a class="footer-nav__link" href="#">Preguntas frecuentes</a></li>
+              <li><a class="footer-nav__link" href="#">Políticas de alquiles y venta</a></li>
+              <li><a class="footer-nav__link" href="#">Términos y condiciones</a></li>
+            </ul>
+          </div>
+
+          <div class="footer-nav footer-grid__item f_middle">
+            <h6 class="footer-nav__title">Encuéntranos</h6>
+            <pre>Cll 00 #0-00, local #0
+CC STUFF, Bucaramanga</pre>
+            <a href="#" class="footer-misc__tiendas">
+              <i class="fas fa-map-marker-alt"></i>
+              <span> Localiza nuestra tienda</span>
+            </a>
+          </div>
+
+  			  <div class="footer-grid__item f_row">© <span class="footer-creditos__year">' 2019 '</span>  Marilyn Moda S.A.</div>
+
+        </div> <!--END FOOTER-GRID-->
       </footer>
 
     </div> <!--END OF CONTAINER-FLUID-->
