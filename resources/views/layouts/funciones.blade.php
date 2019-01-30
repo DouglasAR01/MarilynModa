ad5a05b71c854ae4e2513fdb03744b33d022ce06
TheYellowTieWriter <33265560+TheYellowTieWriter@users.noreply.github.com>
2019-01-30 15:52:10 -0500

    Barra de funciones parcial

---
+++ resources/views/layouts/funciones.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('stylesheets')
+  <link rel="stylesheet" href="{{asset('css/funciones.css')}}">
+@endsection
+
+@include('partials.funcionesEmpleado')
---
+++ resources/views/partials/funcionesEmpleado.blade.php
@@ -0,0 +1,17 @@
+<div class="funciones-container">
+		<a href="{{ url('/') }}" class="navbar-brand">Marilyn<span>Moda</span></a>
+		<div class="dropdown">
+			<button class="btn dropdown-toggle nueva-funcion" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Nuevo</button>
+
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+		    <a class="dropdown-item" href="#">Factura</a>
+		    <a class="dropdown-item" href="#">Gasto</a> 	
+		  </div>
+		</div>
+		<div class="funciones">
+			<a href="#" class="funcion">Facturas</a>
+			<a href="#" class="funcion">Prendas</a>
+			<a href="#" class="funcion">Clientes</a>
+			<a href="#" class="funcion">Gastos</a>
+		</div>
+	</div>
